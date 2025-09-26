6c6f09599ffd2fa3dcc91d6b9ca1ad3007418080
Aswinda Prima Putra <aswinda.pp@gmail.com>
2025-09-26 16:26:42 +0700

    draft project

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b004d73f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s0psoqlrl78"
+    "revision": "0.4s8kb0t4d58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
